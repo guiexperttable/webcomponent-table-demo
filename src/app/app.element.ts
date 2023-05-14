9f60d8ac329ae89481f77f6fb56a3e5f0a41d0a9
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2023-05-14 13:07:10 +0200

    mist

--- src/app/app.element.ts
+++ src/app/app.element.ts
@@ -1,24 +1,38 @@
 import "./app.element.css";
-import { TableModelIf, TableOptions } from "@guiexpert/table";
+import { TableModelAndOptions, TableModelIf, TableOptions } from "@guiexpert/table";
 import { generateSimpleModel } from "@guiexpert/demo-table-models";
-import { TableComponent } from "@guiexpert/webcomponent-table"; // <-- our library
+import { GuiexpertTableComponent } from "@guiexpert/webcomponent-table"; // <-- our library
 
 
 export class AppElement extends HTMLElement {
 
-  public static observedAttributes = [];
+  public static observedAttributes = ['data'];
+
+
+  constructor() {
+    super();
+  }
 
   connectedCallback() {
-    this.innerHTML = `<guiexpert-table style="width:100%;height:100%;background:transparent;margin:0;padding:0;"></guiexpert-table>`;
+    console.info('connectedCallback...');
+    this.innerHTML = `
+      <guiexpert-table-component
+        style="width:100%;height:100%;background:transparent;margin:0;padding:0;"
+        ></guiexpert-table-component>`;
 
-    const table = this.querySelector("guiexpert-table") as TableComponent;
+    const table = this.querySelector("guiexpert-table-component") as GuiexpertTableComponent;
     table.addEventListener("tableReady", (e) => console.info("table api:", (e as CustomEvent).detail));
 
     const tableModel: TableModelIf = generateSimpleModel(1000, 100);
     const tableOptions = new TableOptions();
-    table.setData(tableModel, tableOptions);
+    console.info(table);
+    console.info(tableModel);
+    console.info(typeof table);
+    // table.setData(new TableModelAndOptions(tableModel, tableOptions));
+    table.data = (new TableModelAndOptions(tableModel, tableOptions));
   }
 
 }
 
 customElements.define("webcomponent-table-demo-root", AppElement);
+
